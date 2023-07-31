1a6dec0e6d8cda8761b0708a171e0de866213353
yusfmre <yusfmre0@gmail.com>
2023-07-31 09:11:18 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-})
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// })
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/yusfmre0.github.io/' : '/'
